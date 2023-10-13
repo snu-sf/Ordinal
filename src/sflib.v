dab07d710e2bf0f7882c646bb48c6f463a601236
Jaehyung Lee <jaehyung.lee@sf.snu.ac.kr>
2023-10-13 12:30:38 +0900

    update sflib

--- src/sflib.v
+++ src/sflib.v
@@ -721,7 +721,6 @@ Tactic Notation "extensionalities" ident(a) ident(b) ident(c) ident(d) ident(e) 
 
 (* short for common tactics *)
 
-Tactic Notation "inst" := instantiate.
 Tactic Notation "econs" := econstructor.
 Tactic Notation "econs" int_or_var(x) := econstructor x.
 Tactic Notation "i" := intros.
@@ -784,14 +783,6 @@ Ltac clear_upto H :=
  | clear Hcrr ] end).
 
 Definition _Evar_sflib_ (A:Type) (x:A) := x.
-
-Tactic Notation "hide_evar" int_or_var(n) := let QQ := fresh "QQ" in
-  hget_evar n; intro;
-  lazymatch goal with [ H := ?X |- _] =>
-    set (QQ := X) in *; fold (_Evar_sflib_ X) in QQ; clear H
-  end.
-
-Ltac hide_evars := repeat (hide_evar 1).
 
 Ltac show_evars := repeat (match goal with [ H := @_Evar_sflib_ _ _ |- _ ] => unfold
  _Evar_sflib_ in H; unfold H in *; clear H end).
